182fa8929fc5e75377889f6a277bd4e11dff083f
emre2023 <muhammed8336@gmail.com>
2021-07-29 15:56:04 +0300

    29.07

--- src/CountryDetail.js
+++ src/CountryDetail.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { TextInput, View, Text, StyleSheet,ImageBackground, Image, Button, Alert, Dimensions,TouchableOpacity ,ScrollView} from 'react-native';
+import { TextInput, View, Text, StyleSheet,ImageBackground, Image, Button,Modal,Alert, Dimensions,TouchableOpacity ,ScrollView} from 'react-native';
 import { Icon } from 'react-native-elements';
 import 'react-native-gesture-handler';
-import CheckBox from '@react-native-community/checkbox';
-import CheckboxList from 'rn-checkbox-list';
+import { Checkbox } from 'react-native-paper';
+//import CheckboxList from 'rn-checkbox-list';
 
+import CheckboxFormX from 'react-native-checkbox-form';
 import IGStoryCircle from "react-native-instagram-story-circle";
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage,CardSection } from 'react-native-cards';
 
@@ -15,21 +16,47 @@ import { SearchBar } from 'react-native-elements';
 import { color } from 'react-native-elements/dist/helpers';
 import TripDetail from './components/TripDetail';
 const { width, height } = Dimensions.get("window")
-data = [{ id: 1, name: 'Green Book' }, { id: 2, name: 'Bohemian Rhapsody' }];
+import { CheckBox } from 'react-native-elements';
+
+const mockData = [
+  {
+      label: 'label1',
+      value: 'one'
+  },
+  {
+      label: 'label2',
+      value: 'two'
+  },
+  {
+      label: 'label3',
+      value: 'three'
+  },
+];
+
+
 class CountryDetail extends Component { 
+
 
     state = {
         search: '',
         page: "HomeScreen",
+        checked: false ,
+        checked2: false ,
+        checked3: false ,
+        checked4: false ,
+
     };
     
 
     updateSearch = (search) => {
         this.setState({ search });
     };
- 
+
   render() {  
+     
     const { search } = this.state;
+    
+
     return (  
       <View style = {{ backgroundColor:"gray"}}>  
       <ScrollView>
@@ -87,16 +114,125 @@ class CountryDetail extends Component {
 
              
          
-        
-                <CheckboxList
-  headerName="Movies"
-  theme="red"
-  listItems={data}
-  onChange={({ ids, items }) => console.log('My updated list :: ', ids)}
-  listItemStyle={{ borderBottomColor: '#eee', borderBottomWidth: 1 }}
-  checkboxProp={{ boxType: 'square' }} // iOS (supported from v0.3.0)
-  onLoading={() => <LoaderComponent />}
-/>
+    <View style = {{ flexDirection: "row", justifyContent : "space-around"}}>
+
+    <TouchableOpacity  onPress = {() => {this.setState({ isVisible: true})}} style = {{ flexDirection: "row"}}>
+<Icon
+            
+            name='filter'
+            type='ionicon'
+            color='black'
+            
+          />
+          <Text style = {{ paddingLeft:width*0.02}}>
+            Sort by
+          </Text>
+
+   </TouchableOpacity>  
+ <TouchableOpacity style = {{ flexDirection: "row"}}>
+<Icon
+            
+            name='funnel-outline'
+            type='ionicon'
+            color='black'
+          />
+           <Text style = {{ paddingLeft:width*0.02}}>
+            Filter
+          </Text>
+
+
+   </TouchableOpacity>  
+
+   
+</View>
+
+<Modal   
+ 
+            
+          animationType = {"fade"}  
+          
+          transparent = {true}  
+          visible = {this.state.isVisible}  
+          onRequestClose = {() =>{ console.log("Modal has been closed.") } }>  
+          {/*All views of Modal*/}  
+              <View style = {styles.modal}>  
+              <View style = {{ flexDirection :"row"}}>
+                <Text style = {{marginRight: width*0.43, fontWeight:"bold"}}> Sort By </Text>
+                
+                <Icon onPress = {() => {  
+                  this.setState({ isVisible:!this.state.isVisible})}}
+            
+            name='close'
+            type='ionicon'
+            color='gray'
+          />
+              </View>
+
+              <View style = {{ flexDirection :"row"}}>
+                <Text style = {{ paddingTop:height*0.024,marginRight: width*0.38}}> No of Trips </Text>
+               <CheckBox
+              
+               uncheckedColor = "green"
+               checkedColor = "green"
+
+            
+            iconRight = {true}
+            checked={this.state.checked}
+            onPress={() => this.setState({checked: !this.state.checked})}
+          />
+              </View>
+              <View style = {{ flexDirection :"row"}}>
+                <Text style = {{ paddingTop:height*0.024,paddingRight: width*0.298}}> Price low to high </Text>
+               <CheckBox
+               uncheckedColor = "green"
+               checkedColor = "green"
+
+            
+            iconRight = {true}
+            checked={this.state.checked2}
+            onPress={() => this.setState({checked2: !this.state.checked2})}
+          />
+              </View>
+              <View style = {{ flexDirection :"row"}}>
+                <Text style = {{ paddingTop:height*0.024,paddingRight: width*0.308}}> Price high to low</Text>
+               <CheckBox
+         uncheckedColor = "green"
+         checkedColor = "green"
+
+            
+            iconRight = {true}
+            checked={this.state.checked3}
+            onPress={() => this.setState({checked3: !this.state.checked3})}
+          />
+              </View>
+              <View style = {{ flexDirection :"row"}}>
+                <Text style = {{ paddingTop:height*0.024,paddingRight: width*0.40}}>Relevance </Text>
+               <CheckBox
+               uncheckedColor = "green"
+               checkedColor = "green"
+
+            
+            iconRight = {true}
+            checked={this.state.checked4}
+            onPress={() => this.setState({checked4: !this.state.checked4})}
+          />
+              </View>
+              
+
+
+            
+              
+              
+             
+
+
+            
+          </View>  
+        </Modal>  
+     
+     
+       
+
        
         </ScrollView>
         
@@ -116,7 +252,20 @@ const styles = {
   height: 100,  
   width: "80%",  
   backgroundColor:"#5ead97"  
-}  
+}  ,
+modal: {  
+  justifyContent: 'center',  
+  alignItems: 'center',   
+  backgroundColor : "white",   
+  height: 300 ,  
+  width: '80%',  
+  borderRadius:10,  
+  borderWidth: 1,  
+  borderColor: '#fff',    
+  marginTop: 80,  
+  marginLeft: 40,  
+   
+   },  
 
 }
 export default CountryDetail;
